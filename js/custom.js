e93ee236cd331951899f99adbd36d30362b2913b
YuliaPol <julia.polyeshko@gmail.com>
2020-06-04 12:12:40 +0300

    User

--- js/custom.js
+++ js/custom.js
@@ -41,7 +41,7 @@ jQuery(function ($) {
             $(this).data('id', chapter_id + 1);
             $(newChapter).appendTo('#box-curse-section');
         });
-        $('.user-update-photo').change( function(e){
+        $('.user-update-photo').change(function (e) {
             var fileName = e.target.files[0].name;
             if (e.target.files && e.target.files[0]) {
                 var reader = new FileReader();
@@ -53,62 +53,73 @@ jQuery(function ($) {
             }
             $(this).parents('.btn').next('.file-name').html(fileName);
         });
-        $('#box-curse-section').on('change', '.upload-chapter-file input', function(e){
+        $('#box-curse-section').on('change', '.upload-chapter-file input', function (e) {
             var fileName = e.target.files[0].name;
             $(this).parents('.btn').next('.file-name').html(fileName);
         });
-    });
-    $('#questions-id_type_question').change( function(e){
-        $('.block-params').css('display','none');
-        $('.type-'+$(this).val()).css('display','block');
-        $('.box-for-question').empty();
-    });
-    $('.form-add-question').on('click', '.add-new-any-question', function(){
-        var idNewQuestion = $(this).data('id');
-        var type = $('#questions-id_type_question').val();
-        if(type == 2){
-            var newQuestion = 
-            '<div class="row pad-ver" id="'+ idNewQuestion +'">'+
-            '    <div class="col-md-12">'+
-            '        <div class="col-md-7">'+
-            '            <input type="text" name="Question[name][' + idNewQuestion + ']" class="form-control" placeholder="Название ответа на вопрос">'+
-            '        </div>'+
-            '        <div class="col-md-1">'+
-            '            <div class="checkbox">'+
-            '                <input name="QuestionSave[mark_true][]" value="' + idNewQuestion +'" id="QuestionSave_' + idNewQuestion + '" class="magic-checkbox" type="checkbox">'+
-            '                <label for="QuestionSave_' + idNewQuestion + '"></label>'+
-            '            </div>'+
-            '        </div>'+
-            '        <div class="col-md-2">'+
-            '            <div class="btn btn-danger  glyphicon glyphicon-remove"'+
-            '            onclick="return popupDeleteQuestionDynamic(&#39;' + idNewQuestion +'&#39;)"'+
-            '            ;=""></div>'+
-            '        </div>'+
-            '    </div>'+
-            '</div>';
-        }
-        else {
-            var newQuestion = 
-            '<div class="row pad-ver" id="'+ idNewQuestion +'">'+
-            '    <div class="col-md-12">'+
-            '        <div class="col-md-7">'+
-            '            <input type="text" name="Question[name][' + idNewQuestion + ']" class="form-control" placeholder="Название ответа на вопрос">'+
-            '        </div>'+
-            '        <div class="col-md-1">'+
-            '            <div class="radio">'+
-            '                <input class="magic-radio" type="radio" name="Question[mark_true][]" id="question_mark_' + idNewQuestion + '" value="' + idNewQuestion + '">'+
-            '                <label for="question_mark_' + idNewQuestion +'"></label>'+
-            '            </div>'+
-            '        </div>'+
-            '        <div class="col-md-2">'+
-            '            <div class="btn btn-danger  glyphicon glyphicon-remove"'+
-            '            onclick="return popupDeleteQuestionDynamic(&#39;' + idNewQuestion +'&#39;)"'+
-            '            ;=""></div>'+
-            '        </div>'+
-            '    </div>'+
-            '</div>';
-        }
+
+
+        $('#questions-id_type_question').change(function (e) {
+            $('.block-params').css('display', 'none');
+            $('.type-' + $(this).val()).css('display', 'block');
+            $('.box-for-question').empty();
+        });
+
+        $('.form-add-question').on('click', '.add-new-any-question', function () {
+            var idNewQuestion = $(this).data('id');
+            var type = $('#questions-id_type_question').val();
+            if (type == 2) {
+                var newQuestion =
+                    '<div class="row pad-ver" id="' + idNewQuestion + '">' +
+                    '    <div class="col-md-12">' +
+                    '        <div class="col-md-7">' +
+                    '            <input type="text" name="Question[name][' + idNewQuestion + ']" class="form-control" placeholder="Название ответа на вопрос">' +
+                    '        </div>' +
+                    '        <div class="col-md-1">' +
+                    '            <div class="checkbox">' +
+                    '                <input name="QuestionSave[mark_true][]" value="' + idNewQuestion + '" id="QuestionSave_' + idNewQuestion + '" class="magic-checkbox" type="checkbox">' +
+                    '                <label for="QuestionSave_' + idNewQuestion + '"></label>' +
+                    '            </div>' +
+                    '        </div>' +
+                    '        <div class="col-md-2">' +
+                    '            <div class="btn btn-danger  glyphicon glyphicon-remove"' +
+                    '            onclick="return popupDeleteQuestionDynamic(&#39;' + idNewQuestion + '&#39;)"' +
+                    '            ;=""></div>' +
+                    '        </div>' +
+                    '    </div>' +
+                    '</div>';
+            }
+            else {
+                var newQuestion =
+                    '<div class="row pad-ver" id="' + idNewQuestion + '">' +
+                    '    <div class="col-md-12">' +
+                    '        <div class="col-md-7">' +
+                    '            <input type="text" name="Question[name][' + idNewQuestion + ']" class="form-control" placeholder="Название ответа на вопрос">' +
+                    '        </div>' +
+                    '        <div class="col-md-1">' +
+                    '            <div class="radio">' +
+                    '                <input class="magic-radio" type="radio" name="Question[mark_true][]" id="question_mark_' + idNewQuestion + '" value="' + idNewQuestion + '">' +
+                    '                <label for="question_mark_' + idNewQuestion + '"></label>' +
+                    '            </div>' +
+                    '        </div>' +
+                    '        <div class="col-md-2">' +
+                    '            <div class="btn btn-danger  glyphicon glyphicon-remove"' +
+                    '            onclick="return popupDeleteQuestionDynamic(&#39;' + idNewQuestion + '&#39;)"' +
+                    '            ;=""></div>' +
+                    '        </div>' +
+                    '    </div>' +
+                    '</div>';
+            }
             $(this).data('id', idNewQuestion + 1);
             $(newQuestion).appendTo('.box-for-question');
+        });
+        $('.btn-favorite').click(function () {
+            $(this).find('span').toggleClass('favorite-color');
+        });
+        $('.href-load').on('click', function(e){
+            var element = $($(this).data('id_signature'));
+            $('html, body').animate({scrollTop: element.offset().top}, 2000);
+            element.children().get(0).play();
+        });
     });
 });
